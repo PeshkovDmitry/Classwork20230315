9c88c5112e48d28a1788d9a1b8ce11335fb91fb0
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-03-15 20:01:16 +0300

    Сделал сортировку по убыванию

--- Program.cs
+++ Program.cs
@@ -18,7 +18,9 @@ void SelectionSort(int[] array)
         int minPosition = i;
         for (int j = i + 1; j < array.Length; j++)
         {
-            if(array[j] < array[minPosition]) 
+            // Если в условии ниже знак "<" - сортировка по возрастанию
+            // Если в условии ниже знак ">" - сортировка по убыванию
+            if(array[j] > array[minPosition]) 
             {
                 minPosition = j;
             }
